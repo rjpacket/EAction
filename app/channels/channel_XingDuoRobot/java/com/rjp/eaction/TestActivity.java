a60f479720908dd6d8968ad305ddbcbcd99552e6
rjpacket <jimbo922@163.com>
2018-09-11 18:42:52 +0800

    update 19

--- app/channels/channel_XingDuoRobot/java/com/rjp/eaction/TestActivity.java
+++ app/channels/channel_XingDuoRobot/java/com/rjp/eaction/TestActivity.java
@@ -1,38 +1,23 @@
 package com.rjp.eaction;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.NetworkInfo;
-import android.net.Uri;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
-import android.view.Surface;
 import android.view.View;
 import android.widget.SeekBar;
 
-import com.google.android.exoplayer2.*;
-import com.google.android.exoplayer2.analytics.AnalyticsCollector;
-import com.google.android.exoplayer2.analytics.AnalyticsListener;
-import com.google.android.exoplayer2.decoder.DecoderCounters;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.metadata.Metadata;
-import com.google.android.exoplayer2.source.*;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.DefaultMediaSourceEventListener;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.rjp.eaction.base.BaseActivity;
 import com.rjp.eaction.ui.app.App;
 import com.rjp.eaction.utils.UpdateService;
 
-import java.io.IOException;
-
 import static com.rjp.eaction.utils.UpdateService.DOWNLOAD_URL;
 
-public class TestActivity extends Activity {
+public class TestActivity extends BaseActivity {
 
     private final String testUrl = "http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46";
     private SimpleExoPlayer mPlayer;
@@ -44,10 +29,23 @@ public class TestActivity extends Activity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
+    }
 
+    @Override
+    protected void networkReload() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    protected void handle() {
         findViewById(R.id.btn_update).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,7 +91,7 @@ public class TestActivity extends Activity {
             }
         });
         mPlayer.prepare(mediaSource);
-        mPlayer.setPlayWhenReady(true);
+//        mPlayer.setPlayWhenReady(true);
     }
 
     private Handler mHandler = new Handler(){
@@ -105,10 +103,4 @@ public class TestActivity extends Activity {
             mHandler.sendEmptyMessageDelayed(0, 300);
         }
     };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mHandler.removeCallbacksAndMessages(null);
-    }
 }
